c3c5d48117d670651a73442bdacdc9fe96e28e06
fp2513 <fp2513@cumc.columbia.edu>
2024-10-29 11:00:42 -0400

    Listcols

--- functions_source/nsduh_table_format.R
+++ functions_source/nsduh_table_format.R
@@ -5,7 +5,8 @@ NSDUH_import = function(html, table_num, drug_name) {
     html_table() %>% 
     nth(table_num) %>% 
     slice(-1) %>% 
-    mutate(drug = drug_name)
+    mutate(drug = drug_name) %>% 
+    select(-contains("P Value"))
   
   return(drug_df)
 }
